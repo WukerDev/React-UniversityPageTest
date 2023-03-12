6daff964166dd4d3528a18b3efc09819a046db37
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-03-12 12:45:03 +0100

    Creation

---
+++ Astral/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
