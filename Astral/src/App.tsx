7323c99fe2af01d158f9024ee1cda06495846053
BuildTools <unconfigured@null.spigotmc.org>
2023-03-12 13:37:30 +0100

    Modified text.

--- Astral/src/App.tsx
+++ Astral/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Kitafqeqfwqfwqwfwqf``aa + React</h1>
+      <h1>Ula śpi</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count `is {count}
