977f587e8f2e2989d10145aad02242f05bd3c1cd
exemptd <117043755+exemptd@users.noreply.github.com>
2023-03-12 15:36:39 +0300

    Added Kit

--- Astral/src/App.tsx
+++ Astral/src/App.tsx
@@ -15,10 +15,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Kitafqeqfwqfwqwfwqf``aa + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count `is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
