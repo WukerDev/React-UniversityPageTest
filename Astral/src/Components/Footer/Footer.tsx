196b8628f8f2a1c3f579ed3f796ed031cc22473c
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-03-13 14:40:35 +0100

    Footer vis update

--- Astral/src/Components/Footer/Footer.tsx
+++ Astral/src/Components/Footer/Footer.tsx
@@ -4,20 +4,27 @@ import IkonyFooter from './ikony_baza'
 function getYear() {
     return new Date().getFullYear();
 }
+//styles config
+//footer
+var stylefoot = "bg-blue-900 dark:bg-slate-900 pb-2"
+//ikony div
+var styleIconDiv = "flex gap-2 align-middle justify-center pt-2"
+//ikony
+var styleIcon = "foot_color ease-in-out transform hover:scale-125 transition duration-500"
+//kolor ikon
+var styleIconColor = "foot_color" // aby edytować kolor ikon, edytuj klasę w pliku Footer.scss
 //export funkcji do App.tsx, przy zmianie nazwy pamiętać o tym samym w App.tsx
 export default function Footer() {
     return (    
  //footer body 
-<footer className="bg-blue-900 dark:bg-slate-900 pb-2">
+<footer className={stylefoot}>
 {/*div z ikonami, centruje się, tutaj zmieniamy gap między ikonami i padding*/}
-<div className="flex gap-2 align-middle justify-center pt-2" > 
+<div className={styleIconDiv} > 
 {/*Automatic icon generator*/}
 {/*Aby dodać nowe ikony dodaj je w pliku ikony_baza.tsx*/}
     {IkonyFooter.map((ikona: any) => (
-        <a href={ikona.href} target="_blank" className="foot_color ease-in-out transform hover:scale-125 transition duration-500">
-            <img className='foot_color' src={ikona.icon} /></a>))}</div>
+        <a href={ikona.href} target="_blank" className={styleIcon}>
+            <img className={styleIconColor} src={ikona.icon} /></a>))}</div>
 {/*Copyright notice, z automatycznym update roku*/}
 <a className="text-gray-400">©{getYear()} Uniwersytet Kazimierza Wielkiego</a>  
-</footer>
-    )
-}
+</footer>)}
