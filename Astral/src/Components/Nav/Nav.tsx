42e0aa3452c1689ea64fde062c53453da355b908
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-03-13 19:44:51 +0100

    Navbar Styles rework 1/2

--- Astral/src/Components/Nav/Nav.tsx
+++ Astral/src/Components/Nav/Nav.tsx
@@ -2,23 +2,33 @@ import { Fragment } from 'react'
 import { useState } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import ukwlogo from '../ukw.svg'
+import ukwlogo from './ukw.svg'
 
 //liczba notyfikacji, backend do napisania TODO:
-  let NotifyAmmoun = 5;
-
+let NotifyAmmoun = 5;
+//aby dodawać nowe zakładki, dodaj je w tablicy navigation
 const navigation = [
-  { name: 'Główna', href: '/', current: true },
-  { name: 'Galeria', href: '/Galeria', current: false },
-  { name: 'Kontakt', href: '/Kontakt', current: false },
-  { name: 'FAQ', href: '/FAQ', current: false },
-]
+    //dodawać nowe zakładki w formacie { name: 'nazwa zakładki', href: 'link do zakładki' },
+    { name: 'Główna', href: '/' },
+    { name: 'Galeria', href: '/Galeria' },
+    { name: 'Kontakt', href: '/Kontakt'  },
+    { name: 'FAQ', href: '/FAQ'  },
+    //href jest też wykorzystywane do określenia wartości current trochę niżej, zmieniając href, zmienia się też current
+  ]
+//STYLES CONFIG tutaj edytujemy style navbara poprzez dodawanie klas
+var StyleNavDisclosure = "bg-blue-900 dark:bg-slate-900 sticky top-0"
+var StyleNavDisclosureButton = "inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+var StyleNavDisclosureIcon = "block h-6 w-6"
+var StyleNavDisclosureLogo = "block h-8 w-auto lg:hidden bg-white rounded-full p-0.5"
+var StyleNavDisclosureMenuButton = "flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+var StyleNavDisclosureMenuItems = "absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Test() {
+export default function NavBar() {
+
 const [picture, setPicture] = useState('https://www.w3schools.com/howto/img_avatar.png');
 const pictures = ['https://www.w3schools.com/howto/img_avatar.png', 'https://www.w3schools.com/howto/img_avatar2.png'];
 function togglePicture(){
@@ -29,34 +39,25 @@ function togglePicture(){
   }
 }
   return (
-    <Disclosure as="nav" className="bg-blue-900 dark:bg-slate-900 sticky top-0">
+    <Disclosure as="nav" className={StyleNavDisclosure}>
       {({ open }) => (
         <>
           <div className="mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className={StyleNavDisclosureButton}>
                   <span className="sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className={StyleNavDisclosureIcon} aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className={StyleNavDisclosureIcon} aria-hidden="true" />
                   )}
-                </Disclosure.Button>
-              </div>
+                </Disclosure.Button> </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="block h-8 w-auto lg:hidden bg-white rounded-full p-0.5"
-                    src={ukwlogo}
-                    alt="Your Company"
-                  />
-                  <img
-                    className="hidden h-8 w-auto lg:block bg-white  rounded-full p-0.5"
-                    src={ukwlogo}
-                    alt="Your Company"
-                  />
+                  <img className={StyleNavDisclosureLogo} src={ukwlogo} alt="Uniwersytet Kazimierza Wielkiego" />
+                  <img className={StyleNavDisclosureLogo} src={ukwlogo} alt="Uniwersytet Kazimierza Wielkieg" />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
@@ -65,10 +66,10 @@ function togglePicture(){
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'dark:bg-slate-800 bg-blue-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.href === window.location.pathname ? 'dark:bg-slate-800 bg-blue-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={item.href === window.location.pathname ? 'page' : undefined}
                       >
                         {item.name}
                       </a>
@@ -90,7 +91,7 @@ function togglePicture(){
                 {/* Profile dropdown TODO:Podział na loged i not loged*/}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className={StyleNavDisclosureMenuButton}>
                       <span className="sr-only">Open user menu</span>
                       <img onClick={() => togglePicture()}
                         className="h-8 w-8 rounded-full" 
@@ -108,7 +109,7 @@ function togglePicture(){
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className={StyleNavDisclosureMenuItems}>
                       <Menu.Item>
                         {({ active }) => (
                           <a
@@ -174,10 +175,10 @@ function togglePicture(){
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'dark:bg-gray-800 bg-blue-800 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.href === window.location.pathname ? 'dark:bg-gray-800 bg-blue-800 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={item.href === window.location.pathname ? 'page' : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>
