850ce88c824be4906b7e82fa50affc693f681854
WukerDev <wiktor.kozakowski@student.ukw.edu.pl>
2023-03-13 09:03:14 +0100

    Footer animations

--- Astral/src/Components/Footer/Footer.tsx
+++ Astral/src/Components/Footer/Footer.tsx
@@ -6,17 +6,22 @@ import instagramlogo from  './iconmonstr-instagram-15.svg'
 import youtubelogo from  './iconmonstr-youtube-10.svg'
 import twitterlogo from  './iconmonstr-twitter-5.svg'
 
+function getYear() {
+    return new Date().getFullYear();
+}
+
 export default function Test2() {
     return (    
-<footer className="bg-tramsparent">
-<div className="flex gap-2 align-middle justify-center " > 
-    <a href="https://www.facebook.com/ukwbydgoszcz" target="_blank" className="foot_color"><img src={facebooklogo} /></a>
-    <a href="https://www.instagram.com/ukwbydgoszcz" target="_blank" className="foot_color"><img src={instagramlogo} className=""/></a>
-    <a href="https://twitter.com/UKWBydgoszcz" target="_blank" className="foot_color"><img src={twitterlogo} className=""/></a>
-    <a href="https://www.linkedin.com/school/uniwersytet-kazimierza-wielkiego/?originalSubdomain=pl" target="_blank" className="foot_color"><img src={linkedinlogo} className=""/></a>
-    <a href="https://www.youtube.com/ukwbydgoszcz" target="_blank" className="foot_color"><img src={youtubelogo} className=""/></a>
+        
+<footer className="bg-gradient-to-l from-blue-900 via-blue-800 to-blue-900 pb-2">
+<div className="flex gap-2 align-middle justify-center pt-2" > 
+    <a href="https://www.facebook.com/ukwbydgoszcz" target="_blank" className="foot_color ease-in-out transform hover:scale-125 transition duration-500"><img src={facebooklogo} /></a>
+    <a href="https://www.instagram.com/ukwbydgoszcz" target="_blank" className="foot_color ease-in-out transform hover:scale-125 transition duration-500"><img src={instagramlogo} className=""/></a>
+    <a href="https://twitter.com/UKWBydgoszcz" target="_blank" className="foot_color ease-in-out transform hover:scale-125 transition duration-500"><img src={twitterlogo} className=""/></a>
+    <a href="https://www.linkedin.com/school/uniwersytet-kazimierza-wielkiego/?originalSubdomain=pl" target="_blank" className="foot_color ease-in-out transform hover:scale-125 transition duration-500"><img src={linkedinlogo} className=""/></a>
+    <a href="https://www.youtube.com/ukwbydgoszcz" target="_blank" className="foot_color ease-in-out transform hover:scale-125 transition duration-500"><img src={youtubelogo} className=""/></a>
 </div>
-<a className="">©2023 ukw bydgoszcz</a>  
+<a className="">©{getYear()} Uniwersytet Kazimierza Wielkiego</a>  
 </footer>
     )
 }
