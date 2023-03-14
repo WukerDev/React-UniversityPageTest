2ca97d71cb13053138e6ac4fd23cbe45ee7939a9
WukerDev <wiktor.kozakowski@student.ukw.edu.pl>
2023-03-14 10:59:17 +0100

    Minor look update

--- Astral/src/routes/error-page.tsx
+++ Astral/src/routes/error-page.tsx
@@ -15,7 +15,7 @@ export default function ErrorPage() {
         
       </p>
       <img src={ukw_eeror} />
-      <a href="/" className="text-4xl text-sky-500 hover:text-sky-800" >Powrót na stronę główną</a>
+      <a href="/" className="text-4xl var StyleNavDisclosureButton inline-flex items-center justify-center rounded-md p-3 text-gray-400 bg-gray-800  hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white " >Powrót na stronę główną</a>
     </div>
   );
 }
