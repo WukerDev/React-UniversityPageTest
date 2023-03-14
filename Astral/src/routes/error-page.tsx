100d89f25bf0c893e1ab9d26b0e42548b8c28a7b
Zimnior20 <zimnior12xddd@gmail.com>
2023-03-14 12:24:41 +0100

    Error page styling.

--- Astral/src/routes/error-page.tsx
+++ Astral/src/routes/error-page.tsx
@@ -5,17 +5,20 @@ export default function ErrorPage() {
   const error: any = useRouteError();
 
   return (
-    <div className="grid grid-cols-1 gap-2 place-items-center h-1/2">
-      <h1 className="">O nie, mamy problem!</h1>
+    <div className="gap-2 grid grid-cols-1 h-1/2 my-5">
+      <div className="flex fl flex-col px-40 text-black dark:text-white text-2xl font-bold items-center py-5 mx-20 bg-violet-100 border-l-8 border-violet-700 dark:bg-zinc-800 dark:border-zinc-300 ">
+      <img src={ukw_eeror} className=""/>
+      <h1 className="">O nie, jesteśmy zgubieni!</h1>
       <pre>{error.message}</pre>
       <p>
-        <i>{error.status} {error.statusText || error.message}</i>
+        <i>{error.status}: {error.statusText || error.message}</i>
         
         {error.status === 404  ? null  : null}
         
       </p>
-      <img src={ukw_eeror} />
-      <a href="/" className="text-4xl var StyleNavDisclosureButton inline-flex items-center justify-center rounded-md p-3 text-gray-400 bg-gray-800  hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white " >Powrót na stronę główną</a>
+      
+      </div>
+      <a href="/" className="text-xl border-red-700 border-l-8 mx-20 bg-red-100 py-2 dark:bg-zinc-700 dark:border-zinc-300">Powrót na stronę główną</a>
     </div>
   );
 }
