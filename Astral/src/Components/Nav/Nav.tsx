5546a4939e2acd256f6fcb822d25573cc417f56d
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-08-07 09:39:43 +0200

    Spinn logo

--- Astral/src/Components/Nav/Nav.tsx
+++ Astral/src/Components/Nav/Nav.tsx
@@ -24,7 +24,7 @@ var StyleNavDisclosure = "bg-blue-900 dark:bg-slate-900 sticky top-0"
 var StyleNavDisclosureButton = "inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 
 var StyleNavDisclosureIcon = "block h-6 w-6"
-var StyleNavDisclosureLogo = "block h-8 w-auto lg:block bg-white rounded-full p-0.5 hover:animate-spin"
+var StyleNavDisclosureLogo = "block h-8 w-auto lg:block bg-white rounded-full p-0.5"
 
 var StyleNavDisclosureMenuButton = "flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 
