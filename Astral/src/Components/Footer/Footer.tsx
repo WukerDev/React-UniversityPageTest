ffe403c347c8c9985073011805ce0a01ff3075a8
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-08-07 09:36:31 +0200

    Changes

--- Astral/src/Components/Footer/Footer.tsx
+++ Astral/src/Components/Footer/Footer.tsx
@@ -17,14 +17,6 @@ var styleIconColor = "foot_color" // aby edytować kolor ikon, edytuj klasę w p
 export default function Footer() {
     return (    
  //footer body 
-<footer className={stylefoot}>
-{/*div z ikonami, centruje się, tutaj zmieniamy gap między ikonami i padding*/}
-<div className={styleIconDiv} > 
-{/*Automatic icon generator*/}
-{/*Aby dodać nowe ikony dodaj je w pliku ikony_baza.tsx*/}
-    {IkonyFooter.map((ikona: any) => (
-        <a key={ikona.id} href={ikona.href} target="_blank" className={styleIcon}>
-            <img className={styleIconColor} src={ikona.icon} /></a>))}</div>
-{/*Copyright notice, z automatycznym update roku*/}
-<a className="text-gray-400">©{getYear()} Uniwersytet Kazimierza Wielkiego</a>  
+<footer>
+
 </footer>)}
--- Astral/src/Components/Nav/Nav.tsx
+++ Astral/src/Components/Nav/Nav.tsx
@@ -83,66 +83,9 @@ export default function NavBar() {
             </div>
 
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <Menu as="div" className="relative ml-3">
-                <Menu.Button className="rounded-full bg-blue-900 dark:bg-slate-900 p-1 text-gray-400 hover:text-white focus:outline-none hover:transition focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  {(NotifyAmmoun != 0 ? <span className="absolute -mt-1.5  rounded-full animate-pulse bg-red-700 py-0 px-1.5 text-xs text-white">{NotifyAmmoun}</span> : null)}
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </Menu.Button>
 
-                {/* Renderowanie powiadomień na dzwoneczku na podstawie ilości elementów w obiekcie Notifications z NotificationsData.tsx */}
-
-                {
-                  NotifyAmmoun > 0 ? /* Jeśli jest więcej niż 0 powiadomień, to wyświetl je w liście. */
-                    <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100">
-                      <Menu.Items className="p-2 absolute right-0 z-10 mt-2 w-64 origin-top-right rounded-md bg-white dark:bg-slate-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="max-h-64 overflow-x-auto">
-                          {Notifications.map((item) => (
-                            <Menu.Item key={item.sender}><div className="dark:bg-slate-900 bg-blue-500 mb-2 mt-2 py-2 rounded-xl break-words">
-                              <div className="flex-row">
-                                <p className="dark:text-sky-400 text-xs text-sky-200" >{item.dateTime}</p>
-                                <p className="dark:text-sky-400 text-xs text-sky-200" >{item.sender}</p>
-                                <p className="  text-center text-sm">{item.message}</p></div>
-                            </div></Menu.Item>))}
-                        </div></Menu.Items>
-                    </Transition>
-
-                    : /* NIE DOTYKAĆ DWUKROPKA (SZCZEGÓLNIE TY WIKTOR) */
-                    /* Jeśli jest 0 lub mniej elementów w liście (warto być na 100% pewnym) to wyświetl listę z informacją o braku powiadomień. */
-                    <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100">
-                      <Menu.Items className="p-2 absolute right-0 z-10 mt-2 w-64 origin-top-right rounded-md bg-white dark:bg-slate-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          <div className="dark:bg-slate-900 bg-blue-500 mb-2 mt-2 py-2 rounded-xl">
-                            <div className="flex-row">
-                              <p className="  text-center text-sm">{NoNotifData}</p></div>
-                          </div>
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                }
-              </Menu>
               {/* Profile dropdown TODO:Podział na loged i not loged*/}
-              <Menu as="div" className="relative ml-3">
-                <div>
-                  <Menu.Button className={StyleNavDisclosureMenuButton}>
-                    <span className="sr-only">Open user menu</span>
-                    <img onClick={() => togglePicture()} className={ProfilePictureStyle} src={picture} alt="Profile Picture" />
-                  </Menu.Button>
-                </div>
-                <Transition as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
-                  <Menu.Items className={StyleNavDisclosureMenuItems}>
-                    {MenuData.map((item) => (
-                      <Menu.Item>
-                        {({ active }) => (
-                          <NavLink key={item.id}
-                            to={item.href}
-                            className={classNames(active ? "bg-gray-100 dark:bg-slate-900" : '', 'block px-4 py-2 text-sm text-slate-800 dark:text-slate-50')}>
-                            {item.name}</NavLink>)}
-                      </Menu.Item>))}
-                  </Menu.Items>
-                </Transition>
-              </Menu>
+
             </div>
           </div>
         </div>
