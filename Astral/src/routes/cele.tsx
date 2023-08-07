b639f33f1f1bc4cf0da8e06ea05775b822a8c14f
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-08-07 12:34:49 +0200

    zdjęcia

--- Astral/src/routes/cele.tsx
+++ Astral/src/routes/cele.tsx
@@ -1,4 +1,10 @@
-import Imiona from "../Components/BazaImion"
+
+import cele1 from '../../public/podwozieProj.png'
+import cele2 from '../../public/przekladniaProj.png'
+import cele3 from '../../public/liczOs1.png'
+import cele4 from '../../public/liczOs2.png'
+import cele5 from '../../public/cele5.png'
+
 
 
 export default function Cele() {
@@ -13,11 +19,19 @@ export default function Cele() {
       </p>
       <p className="text-xl mb-4">Wybrane cel szczegółowe:</p>
       <ul className="list-disc list-inside">
-        <li>"Konstrukcja programowalnej, mobilnej platformy kołowej do dezynfekcji pomieszczeń"</li>
-        <li>"Opracowanie systemu liczenia osób wchodzących do pomieszczenia oraz korelacja tej ilości z użyciem dozownika płynu dezynfekującego”</li>
+        <li>Konstrukcja programowalnej, mobilnej platformy kołowej do dezynfekcji pomieszczeń</li>
+        <img src={cele1} alt="cele1" />
+        <img src={cele2} alt="cele2" />
+
+        <li>Opracowanie systemu liczenia osób wchodzących do pomieszczenia oraz korelacja tej ilości z użyciem dozownika płynu dezynfekującego</li>
+        <img src={cele3} alt="cele3" />
+        <img src={cele4} alt="cele4" />
+        <li>Opracowanie systemu informatycznego</li>
+        <img src={cele5} alt="cele5" />
+
       </ul>
 
-      <h1 className="text-4xl font-bold mt-10 mb-4">Opracowanie systemu informatycznego</h1>
+    
     </div>
     </>
   )
