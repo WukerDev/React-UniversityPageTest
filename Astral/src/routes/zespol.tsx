84155dd3947be6a887c3ebc62e25436ea7e3b474
saulgoodman20 <kubusmateusz1@gmail.com>
2023-08-07 11:57:48 +0200

    aaaaa

--- Astral/src/routes/zespol.tsx
+++ Astral/src/routes/zespol.tsx
@@ -10,7 +10,7 @@ export default function Zespol() {
       <h1 className="text-4xl font-bold mt-10 mb-4">Zespół</h1>
       <p className="text-xl mb-4">
         Studenci (rekrutowani od 1 do 4 roku) Informatyki i Mechatroniki UKW Bydgoszczy - Koło Naukowe Inkubator Technologiczny: 
-        <a className="text-blue-500 hover:underline" href="https://informatyka.ukw.edu.pl/jednostka/wydzial-informatyki/kola/58694/inkubator-technologiczny">https://informatyka.ukw.edu.pl/jednostka/wydzial-informatyki/kola/58694/inkubator-technologiczny</a>
+        <a target="_blank" className="text-blue-500 hover:underline" href="https://informatyka.ukw.edu.pl/jednostka/wydzial-informatyki/kola/58694/inkubator-technologiczny">https://informatyka.ukw.edu.pl/jednostka/wydzial-informatyki/kola/58694/inkubator-technologiczny</a>
       </p>
     </div>
     </>
