60f6cc895a7753987e3d2e3dbc9bb9324994bab2
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-08-07 09:15:17 +0200

    XD

--- Astral/src/Components/Nav/NavData.tsx
+++ Astral/src/Components/Nav/NavData.tsx
@@ -5,5 +5,6 @@ const NavData = [
     { id: 1, name: 'Główna', href: '/' },
     { id: 2, name: 'Zespół', href: '/Zespol' },
     { id: 3, name: 'Cele Projektu', href: '/Cele'  },
+    { id: 3, name: 'Opracowane Rozwiązania', href: '/Rozwiazania'  },
   ]
 export default NavData;
