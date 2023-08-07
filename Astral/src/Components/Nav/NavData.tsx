f99c5afa4c64d1de6af4c106082cb21d2ddd059a
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-08-07 08:52:23 +0200

    3 strony

--- Astral/src/Components/Nav/NavData.tsx
+++ Astral/src/Components/Nav/NavData.tsx
@@ -3,8 +3,7 @@
 const NavData = [
     //dodawać nowe zakładki w formacie { name: 'nazwa zakładki', href: 'link do zakładki' },
     { id: 1, name: 'Główna', href: '/' },
-    { id: 2, name: 'Galeria', href: '/Gallery' },
-    { id: 3, name: 'Kontakt', href: '/Contact'  },
-    { id: 4, name: 'FAQ', href: '/FAQ'  },
+    { id: 2, name: 'Zespół', href: '/Zespol' },
+    { id: 3, name: 'Cele Projektu', href: '/Cele'  },
   ]
 export default NavData;
