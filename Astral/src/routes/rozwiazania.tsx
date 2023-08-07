4c37a36eb06f0e41930df237dd3f259abe4be5ff
Wiktor Kozakowski <108416911+WukerDev@users.noreply.github.com>
2023-08-07 10:24:53 +0200

    Rozwiązania

--- Astral/src/routes/rozwiazania.tsx
+++ Astral/src/routes/rozwiazania.tsx
@@ -3,9 +3,15 @@ import Imiona from "../Components/BazaImion"
 export default function Rozwiazania() {
   return (
     <>
-      <div className="text-5xl">
-        <p>Rozwiązania</p>
-      </div>
+      <div className="p-10">
+      <h1 className="text-4xl font-bold mb-4">Opracowane rozwiązania</h1>
+      <p className="text-xl mb-4">
+        Poprzez prace studentów oraz kadry koła naukowego Inkubator Technologiczny UKW, opracowano platformę mobilną, o napędzie elektrycznym, która zdolna jest do przenoszenia systemu dezynfekcji. Opracowane rozwiązanie zostało skonstruowane z wykorzystaniem niżej opisanych rozwiązań. W zakresie:
+      </p>
+      <ul className="list-disc list-inside">
+        <li>konstrukcji: aluminiowych profili konstrukcyjnych, opracowanych i w części gotowych elementów montażowych w technologii druku 3D</li>
+      </ul>
+    </div>
     </>
   )
 }
