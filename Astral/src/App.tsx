7cda0d951c979e5cad684b9333eb12a042d105dd
saulgoodman20 <kubusmateusz1@gmail.com>
2023-08-18 03:56:12 +0200

    Replaced router types

--- Astral/src/App.tsx
+++ Astral/src/App.tsx
@@ -12,28 +12,27 @@ import NavBar from './Components/Nav/Nav'
 import Footer from './Components/Footer/Footer'
 
 //REACT-ROUTER
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, createHashRouter } from 'react-router-dom'
 
   //TODO: Dodanie funkcji zmiany motywu do przycisku
   //https://tailwind-elements.com/docs/standard/getting-started/dark-mode/
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path="/" element={<Root />} errorElement={<ErrorPage />}>,
-      <Route index element={<Home />} />,
-      <Route path="/Zespol" element={<Zespol />} />
-      <Route path="/Cele" element={<Cele />} />
-      <Route path="/Rozwiazania" element={<Rozwiazania />} />
-    </Route>
-    </>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <>
+//     <Route path="/" element={<Root />} errorElement={<ErrorPage />}>,
+//       <Route index element={<Home />} />,
+//       <Route path="/Zespol" element={<Zespol />} />
+//       <Route path="/Cele" element={<Cele />} />
+//       <Route path="/Rozwiazania" element={<Rozwiazania />} />
+//     </Route>
+//     </>
+//   )
+// );
 function App() {
   return (
     <>
-    <RouterProvider router={router} />
     </>
   )
 }
--- Astral/src/main.tsx
+++ Astral/src/main.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import './App.scss'
+
+import {
+  createHashRouter, RouterProvider, createRoutesFromElements, Route
+} from 'react-router-dom';
+import Root from './routes/root'
+import ErrorPage from './routes/error-page';
+import Home from './routes/home';
+import Zespol from './routes/zespol';
+import Cele from './routes/cele';
+import Rozwiazania from './routes/rozwiazania';
+
+
+
+const router = createHashRouter(
+  createRoutesFromElements(
+    <>
+    <Route path="/" element={<Root />} errorElement={<ErrorPage />} >,
+      <Route index element={<Home />} />,
+      <Route path="/Zespol" element={<Zespol />} />,
+      <Route path="/Cele" element={<Cele />} />,
+      <Route path="/Rozwiazania" element={<Rozwiazania />} />,
+    </Route>
+    </>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
